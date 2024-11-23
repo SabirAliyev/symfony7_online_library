7438edd5bd69c1e3729966fc272bb6fb76942083
Sabir Aliyev (local test env) <sabir.aliyev.itdep@gmail.com>
2024-11-23 11:29:54 +0400

    Save new book from form

--- src/Controller/BookListController.php
+++ src/Controller/BookListController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BookListController extends AbstractController
 {
-    #[Route('/book-list', name: 'app_book-list')]
+    #[Route('/book-list', name: 'app_book_list')]
     public function index(BookRepository $books) : Response
     {
         return $this->render('index.html.twig', [
@@ -28,14 +31,36 @@ class BookListController extends AbstractController
     }
 
     #[Route('/book-add', name: 'app_book_add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
         $book = new Book();
+        $user = $entityManager->getRepository(User::class)->find(1);
+        $book->setAddedBy($user);
+
         $form = $this->createFormBuilder($book)
             ->add('title')
             ->add('author')
+            ->add('genre')
+            ->add('description')
             ->add('submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $book->setCreatedAt(new \DateTime());
+
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            // Add a flash
+            $this->addFlash('success', 'New Book added successfully!');
+            // Redirect
+            return $this->redirectToRoute('app_book_list');
+        } else {
+            $this->addFlash('error', 'Something went wrong!');
+        }
 
         return $this->render('add.html.twig', [
             'form' => $form
